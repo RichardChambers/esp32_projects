ccbbc5d71ccf824552a662b04d994e5f29e8e662
Richard Chambers <rickchambers.geo@gmail.com>
2020-08-08 08:55:52 -0400

    update the blink_esp32 sketch nd the DelayState library source
    I have modified the DelayState library in DelayState.h and
    DelayState.cpp in folder Lib source so that the DelayClass is now using
    the StateClass for all state related functionality. The idea is to
    separate out the Delay functionality which may or may not use the State
    functionality and the State functionality which can now be used without
    the Delay functionality.
    
    Now using three LEDs for blinking at different rates, two on a
    breadboard and the internal LED on pin 2.
    
    Have also added code to monitor a momentary contact press button which
    will detect the first button press then count button presses for five
    seconds.
    
    There is a problem with the current button press functionaity in that we
    are sometimes seeing multiple button press indications when there was
    only one press. We need to added debounce functionality to eliminate
    spurious button press indications.

--- blink_esp32/README.md
+++ blink_esp32/README.md
@@ -1,2 +1,7 @@
 Blink LEDs in different rhythms using the DelayState library to specify delays for pin HIGH, to turn on the LED, and LOW, to turn off the LED, along with a state
 machine to know how to transition to a new state when the delay for the current state expires.
+
+The source code expects three LEDs for blinking: two LEDs on the breadboard connected to pins on the ESP32 and the third
+LED is the builtin LED on pin 2.
+
+In addition, the source code expects a momentary contact button which is used as a user interface.
--- blink_esp32/blink_esp32.ino
+++ blink_esp32/blink_esp32.ino
@@ -8,7 +8,7 @@ static unsigned long ulLoopCount = 0;
 
 int TaskDelayPin (DelayClass &delayPin, int delayPinTiming[], int xPin)
 {
-    // The following state machine flashes the designated LED
+  // The following state machine flashes the designated LED
   // by turning it on for some duration then turning it off
   // for a second duration.
   switch (delayPin.CheckDelayState()) {
@@ -78,9 +78,22 @@ int TaskDelaySerial (DelayClass &delaySerial)
 
 // ============================================================
 
+#define MYPIN_13
+#define MYPIN_6
+
+// pin 2 is same as D2 which is also linked to builtin LED
+// pin 4 is same as D4
+// pin 22 is same as D22
+// pin 23 is same as D23
+
 int myPin = LED_BUILTIN; // 4;
 int myPin_4 = 4;
-int myPin_13 = 5;
+#if defined(MYPIN_13)
+int myPin_13 = 22;
+#endif
+#if defined(MYPIN_6)
+int myPin_6 = 23;
+#endif
 
 static DelayClass DelayPin;
 static int        DelayPinTiming[] = {1000, 500, 1000, 500};
@@ -89,23 +102,18 @@ static int        DelayPin_4Timing[] = {500, 1000, 500, 1000};
 static DelayClass DelayPin_13;
 static int        DelayPin_13Timing[] = {1000, 500, 500, 1000};
 
+static DelayClass DelayPin_6 (5000, 0);
+
 static DelayClass DelaySerial1(500, 0, 71);
 static DelayClass DelaySerial2(750, 0, 72);
 static DelayClass DelaySerial3(250, 0, 73);
 
+static int iReadSave = LOW;
+
+
 void setup() {
   // put your setup code here, to run once:
-
-  DelayPin = DelayClass(1000, 0);
-  pinMode (myPin, OUTPUT);
-  digitalWrite(myPin, LOW);    // initialize the pin to LOW to turn off LED
-
-  pinMode (myPin_4, OUTPUT);
-  digitalWrite(myPin_4, LOW);    // initialize the pin to LOW to turn off LED
-
-  pinMode (myPin_13, OUTPUT);
-  digitalWrite(myPin_13, LOW);    // initialize the pin to LOW to turn off LED
-
+  
 #if 1
   // serial monitor at 9600 rather than 115200
   // running serial at 115200 seems to interfere with WiFi
@@ -113,6 +121,41 @@ void setup() {
 #else
   // serial monitor at 115200 rather than 9600
   Serial.begin(115200);
+#endif
+
+  delay(500);    // give Serial to initialize and setup.
+  
+  Serial.println("begin setup()");
+
+#if defined(MYPIN_13)
+  Serial.print ("   MYPIN_13 defined myPin_13 = ");
+  Serial.println (myPin_13);
+#endif
+
+  DelayPin = DelayClass(1000, 0);
+  
+  pinMode (myPin, OUTPUT);
+  digitalWrite(myPin, LOW);    // initialize the pin to LOW to turn off LED
+
+  pinMode (myPin_4, OUTPUT);
+  digitalWrite(myPin_4, LOW);    // initialize the pin to LOW to turn off LED
+
+#if defined(MYPIN_13)
+  pinMode (myPin_13, OUTPUT);
+  digitalWrite(myPin_13, LOW);    // initialize the pin to LOW to turn off LED
+#endif
+
+#if defined (MYPIN_6)
+  Serial.print ("   MYPIN_6 defined myPin_6 = ");
+  Serial.println (myPin_6);
+
+  pinMode (myPin_6, INPUT);
+ 
+  Serial.print (" pin myPin_6 iReadSave ");
+  Serial.println (iReadSave);
+  iReadSave = digitalRead (myPin_6);
+  Serial.print (" pin myPin_6 read ");
+  Serial.println (iReadSave);
 #endif
 }
 
@@ -123,7 +166,34 @@ void loop() {
 
   TaskDelayPin (DelayPin, DelayPinTiming, myPin);
   TaskDelayPin (DelayPin_4, DelayPin_4Timing, myPin_4);
+#if defined(MYPIN_13)
   TaskDelayPin (DelayPin_13, DelayPin_13Timing, myPin_13);
+#endif
+
+#if defined (MYPIN_6)
+  int  iRead = digitalRead (myPin_6);
+
+  if (iRead != iReadSave) {
+    if (iRead == HIGH) {
+      if (! DelayPin_6.CheckDelayStart()) {
+        DelayPin_6.ulCounter = 1;
+        DelayPin_6.StartDelay(5000);
+        Serial.print("  start button press count ");
+        Serial.println (DelayPin_6.ulCounter);
+      } else if ( ! DelayPin_6.CheckDelayDone()) {
+        DelayPin_6.ulCounter++;
+      }
+    }
+    iReadSave = iRead;
+    Serial.print (" pin myPin_6 read ");
+    Serial.println (iRead);
+  } 
+  if (DelayPin_6.CheckDelayDone()) {
+      Serial.print (" button pushed count = ");
+      Serial.println (DelayPin_6.ulCounter);
+      DelayPin_6.ClearDelayStart();
+  }
+#endif
 
 //  TaskDelaySerial (DelaySerial1);
 //  TaskDelaySerial (DelaySerial2);
